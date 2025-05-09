61a88f3fcd1e75191df940bf81e9bc25b61e26bc
royinsocool <>
2025-05-09 17:50:45 +0800

    first commit
    # Conflicts:
    #	LICENSE

---
+++ lib/basic_future_observer.dart
@@ -0,0 +1,31 @@
+abstract class BaseFutureObserver {
+  // 所有待处理的任务，由子类实现
+  abstract final Map<String, List<Future>> _storage;
+
+  // 注册eventName
+  void register(String event, [Future callback]);
+
+  // 获取指定已注册的事件
+  List<Future> registerOf(String event);
+
+  // 获取全部已注册的事件
+  Map<String, List<Future>> registers();
+
+  // 注销已注册的eventName事件
+  void unregister(String event, Future callback);
+
+  // 注销已注册事件
+  int unregister0f(String event);
+
+  // 取消全部已注册事件
+  void unregisters();
+
+  // 判断单个是否被注册
+  bool registered(String event);
+
+  // 运行单个或多个事件
+  run([String event]);
+
+  // 等待单个或多个事件执行完毕
+  wait([String event]);
+}
---
+++ lib/future_observer.dart
@@ -0,0 +1,58 @@
+import 'basic_future_observer.dart';
+
+/// A Future State Observer.
+class FutureObserver implements BaseFutureObserver {
+  @override
+  Map<String, List<Future>> _storage = {};
+
+  @override
+  void register(String event, [Future? callback]) {
+    // TODO: implement register
+  }
+
+  @override
+  List<Future> registerOf(String event) {
+    // TODO: implement registerOf
+    throw UnimplementedError();
+  }
+
+  @override
+  bool registered(String event) {
+    // TODO: implement registered
+    throw UnimplementedError();
+  }
+
+  @override
+  Map<String, List<Future>> registers() {
+    // TODO: implement registers
+    throw UnimplementedError();
+  }
+
+  @override
+  run([String event]) {
+    // TODO: implement run
+    throw UnimplementedError();
+  }
+
+  @override
+  void unregister(String event, Future callback) {
+    // TODO: implement unregister
+  }
+
+  @override
+  int unregister0f(String event) {
+    // TODO: implement unregister0f
+    throw UnimplementedError();
+  }
+
+  @override
+  void unregisters() {
+    // TODO: implement unregisters
+  }
+
+  @override
+  wait([String event]) {
+    // TODO: implement wait
+    throw UnimplementedError();
+  }
+}
