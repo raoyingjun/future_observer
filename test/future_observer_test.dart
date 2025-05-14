9fcecd4b743c22d650730af4a7c786845279e575
royinsocool <>
2025-05-14 18:48:26 +0800

    补充测试用例

--- test/future_observer_test.dart
+++ test/future_observer_test.dart
@@ -1,21 +1,16 @@
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:future_observer/future_observer.dart';
 
-import '../lib/future_observer.dart';
-
 void main() {
-  test('adds one to input values', () {
-    // final calculator = Calculator();
-    // expect(calculator.addOne(2), 3);
-    // expect(calculator.addOne(-7), -6);
-    // expect(calculator.addOne(0), 1);
+  Future task(int delay, dynamic result) {
+    return Future.delayed(Duration(seconds: delay), () => result);
+  }
+
+  test('观察及等待异步任务', () async {
+    final observer = FutureObserver();
+    observer.observe(task, (getAsyncUserId) => getAsyncUserId(3, 24167));
+    print('模拟异步请求用户 ID，等待 3 秒');
+    expect(await observer.wait(task), 24167);
+    print('成功获取用户 ID，异步任务完成');
   });
-  FutureObserver futureObserver = FutureObserver();
-  futureObserver.register(
-    event: 'testEvent',
-    callback: () async {
-      print('testEvent callback executed');
-    },
-  ); 
 }
